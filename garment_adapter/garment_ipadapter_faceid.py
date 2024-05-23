d176a8eaf4a89d3a1523b18e3bfd4b5080fc023f
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-05-23 18:57:45 +0900

    refactor: Separate animatediff layers into separate module lists in IPAdapterFaceID_AnimateDiff class

--- garment_adapter/garment_ipadapter_faceid.py
+++ garment_adapter/garment_ipadapter_faceid.py
@@ -800,7 +800,8 @@ class IPAdapterFaceID_AnimateDiff:
         print(ip_animatediff_layers_stores)
         print(torch.load(self.self_ip_path, map_location="cpu").keys(), flush=True)
         ip_animatediff_layers_stores.load_state_dict(torch.load(self.self_ip_path, map_location="cpu"))
-        # ip_layers_stores.to(self.device)
+        ip_layers_stores.to(self.device)
+        ip_animatediff_layers_stores.to(self.device)
 
     def set_ori_adapter(self, unet):
         attn_procs = {}
@@ -1046,7 +1047,8 @@ class IPAdapterFaceIDPlus_AnimateDiff:
         ip_layers_stores.load_state_dict(state_dict["ip_adapter"], strict=False)
         print(torch.load(self.self_ip_path, map_location="cpu").keys(), flush=True)
         ip_animatediff_layers_stores.load_state_dict(torch.load(self.self_ip_path, map_location="cpu"))
-        # ip_layers_stores.to(self.device)
+        ip_layers_stores.to(self.device)
+        ip_animatediff_layers_stores.to(self.device)
 
     @torch.inference_mode()
     def get_image_embeds(self, faceid_embeds, face_image, s_scale, shortcut):
