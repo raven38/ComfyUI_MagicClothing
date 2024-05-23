ba84f3a85f4e2b4db6ec69cee846fbd469191402
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-05-24 01:36:13 +0900

    debug

--- garment_adapter/garment_ipadapter_faceid.py
+++ garment_adapter/garment_ipadapter_faceid.py
@@ -845,7 +845,7 @@ class IPAdapterFaceID_AnimateDiff:
             if isinstance(ip_layers[i], REFAnimateDiffAttnProcessor):
                 ip_animatediff_layers_stores.append(ip_layers[i])
                 ip_animatediff_layers_stores.append(torch.nn.Identity())
-            elif not isinstance(ip_layers[i], AttnProcessor):
+            elif isinstance(ip_layers[i], (REFAttnProcessor, IPAttnProcessor)):
                 ip_layers_stores.append(ip_layers[i])
         ip_layers_stores.load_state_dict(state_dict["ip_adapter"], strict=False)
         print(ip_animatediff_layers_stores)
@@ -1143,7 +1143,7 @@ class IPAdapterFaceIDPlus_AnimateDiff:
             if isinstance(ip_layers[i], REFAnimateDiffAttnProcessor):
                 ip_animatediff_layers_stores.append(ip_layers[i])
                 ip_animatediff_layers_stores.append(torch.nn.Identity())
-            elif not isinstance(ip_layers[i], AttnProcessor):
+            elif isinstance(ip_layers[i], (REFAttnProcessor, IPAttnProcessor)):
                 ip_layers_stores.append(ip_layers[i])
         print('ipadapterfaceid', ip_layers_stores)
         print('state_dict', state_dict["ip_adapter"].keys())
