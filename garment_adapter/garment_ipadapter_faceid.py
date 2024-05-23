3218af443d6347b57b42790c026c9cf6df05ec63
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-05-23 22:56:13 +0900

    Refactor IPAdapterFaceID_AnimateDiff class to simplify attention processor initialization

--- garment_adapter/garment_ipadapter_faceid.py
+++ garment_adapter/garment_ipadapter_faceid.py
@@ -849,9 +849,9 @@ class IPAdapterFaceID_AnimateDiff:
         ip_layers_stores.load_state_dict(state_dict["ip_adapter"], strict=False)
         print(ip_animatediff_layers_stores)
         print(torch.load(self.self_ip_path, map_location="cpu").keys(), flush=True)
-        ip_animatediff_layers_stores.load_state_dict(torch.load(self.self_ip_path, map_location="cpu"))
+        # ip_animatediff_layers_stores.load_state_dict(torch.load(self.self_ip_path, map_location="cpu"))
         ip_layers_stores.to(self.device)
-        ip_animatediff_layers_stores.to(self.device)
+        # ip_animatediff_layers_stores.to(self.device)
 
     def set_ori_adapter(self, unet):
         attn_procs = {}
